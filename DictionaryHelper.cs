538fb38fa3773838e17239bf3737c0625f86fba8
thines01 <thines01@yahoo.com>
2023-07-06 23:30:52 -0500

    Add project files.

---
+++ DictionaryHelper.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace DictionaryHelper
+{
+   /// <summary>
+   /// Returns a default value from a Dictionary
+   /// when the searched-for value is not found.
+   /// </summary>
+   public static partial class CDictionaryHelper
+   {
+      /// <summary>
+      /// Returns a default value from the Dictionary
+      /// when the searched-for value is not found.
+      /// </summary>
+      /// <typeparam name="TValue">TDictionary</typeparam>
+      /// <typeparam name="Tk">TKey</typeparam>
+      /// <typeparam name="Tv">TValue</typeparam>
+      /// <param name="source">Dictionary</param>
+      /// <param name="item">item sought</param>
+      /// <returns>The value of the type returned by the Dictionary or the default.</returns>
+      public static Tv EnsuredValue<T, Tk, Tv>(this T source, Tk item) where T: Dictionary<Tk, Tv>
+      {
+         return (source.ContainsKey(item) ? source[item] : default(Tv));
+      }
+   }
+}
---
+++ ToSafeDictionary.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DictionaryHelper
+{
+   public static partial class CDictionaryHelper
+   {
+      /// <summary>
+      /// Creates a dictionary from an IENumerable of TValue using a user-supplied Key.
+      /// The first value is chosen if duplicates are encountered.
+      /// </summary>
+      /// <typeparam name="TKey">User-supplied key</typeparam>
+      /// <typeparam name="TValue">Generated value for the dictionary</typeparam>
+      /// <param name="lst">Original list of elements</param>
+      /// <param name="keySelector">User-supplied key generator method</param>
+      /// <param name="dic">Dictionary to be modified with target keys and values</param>
+      public static void
+         ToSafeDictionary<TKey, TValue>
+         (this IEnumerable<TValue> lst, Func<TValue, TKey> keySelector, Dictionary<TKey, TValue> dic)
+      {
+         lst.GroupBy(keySelector).ToList().ForEach(grp => dic.Add(grp.Key, grp.First()));
+      }
+
+      /// <summary>
+      /// Returns a dictionary based on the IEnumerable and key generator provided.
+      /// The first value is chosen if duplicates are encountered.
+      /// </summary>
+      /// <typeparam name="TKey">User-supplied key</typeparam>
+      /// <typeparam name="TValue">Generated value for the dictionary</typeparam>
+      /// <param name="lst">Original list of elements</param>
+      /// <param name="keySelector">User-supplied key generator method</param>
+      /// <returns>Dictionary</returns>
+      public static Dictionary<TKey, TValue>
+         ToSafeDictionary<TKey, TValue>
+         (this IEnumerable<TValue> lst, Func<TValue, TKey> keySelector)
+      {
+         return lst.GroupBy(keySelector).ToDictionary(grp => grp.Key, grp => grp.First());
+      }
+   }
+}
